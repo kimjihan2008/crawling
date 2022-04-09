4cdd748ce894d0330da0a59658696998be67c5a9
kimjihan2008 <75594823+kimjihan2008@users.noreply.github.com>
2022-04-09 09:44:18 +0900

    Update crawling.py

--- crawling.py
+++ crawling.py
@@ -1,11 +1,11 @@
 import urllib.request
 from bs4 import BeautifulSoup
 
-url = 'https://search.naver.com/search.naver?sm=tab_hty.top&where=view&query=%EB%94%94%EB%AF%B8%EA%B3%A0&oquery=%EB%94%94%EB%AF%B8%EA%B3%A0+%EC%9E%85%EC%8B%9C&tqi=hCGFAlp0Jy0ssE8sKeKssssst6G-024998&mode=normal'
+url = #찾고 싶은 url
 html = urllib.request.urlopen(url).read()
 soup = BeautifulSoup(html, 'html.parser')
 
-title = soup.find_all(class_ = 'api_txt_lines total_tit _cross_trigger')
+title = soup.find_all(class_ = '클래스이름')
 
 data = []
 
